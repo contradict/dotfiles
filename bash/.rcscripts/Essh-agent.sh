363d82a00f17f8d6ece6ebf6599bf5557ee024f9
contradict <contradict@gmail.com>
2016-06-17 07:17:09 -0700

    Handle missing .ssh directory

--- bash/.rcscripts/Essh-agent.sh
+++ bash/.rcscripts/Essh-agent.sh
@@ -1,8 +1,14 @@
-SSH_ENV="$HOME/.ssh/environment"
+SSH_DIR="$HOME/.ssh"
+SSH_ENV="${SSH_DIR}/environment"
 
 function start_agent {
     echo "Initializing new SSH agent..."
-    touch $SSH_ENV
+    if [ ! -d ${SSH_DIR} ]; then
+        echo "Creating new SSH directory"
+        mkdir -p ${SSH_DIR}
+        chmod 600 ${SSH_DIR}
+    fi
+    touch ${SSH_ENV}
     chmod 600 "${SSH_ENV}"
     /usr/bin/ssh-agent | sed '/^echo/d' > "${SSH_ENV}"
     . "${SSH_ENV}" > /dev/null
