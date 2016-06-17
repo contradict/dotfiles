1be8c919be80c69eb871bf70ce011a5b4eaf7b3b
contradict <contradict@gmail.com>
2016-06-17 08:20:30 -0700

    Make ros enable script safer

--- bash/.rcscripts/Eros.sh
+++ bash/.rcscripts/Eros.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-. /opt/ros/indigo/setup.bash
-export ROS_PARALLEL_JOBS=-j8
-export ROSLAUNCH_SSH_UNKNOWN=1
-
+if [ -d /opt/ros/indigo ]; then
+    . /opt/ros/indigo/setup.bash
+    export ROS_PARALLEL_JOBS=-j8
+    export ROSLAUNCH_SSH_UNKNOWN=1
+fi
