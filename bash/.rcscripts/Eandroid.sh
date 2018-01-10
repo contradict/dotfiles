a8528fe8e18889b9c21a601fd0499cf00a9c2d7e
contradict <contradict@gmail.com>
2018-01-10 15:16:19 +0100

    Only enable android sdk if present

--- bash/.rcscripts/Eandroid.sh
+++ bash/.rcscripts/Eandroid.sh
@@ -1,5 +1,9 @@
 ANDROID_VER=22.0.1
-export PATH=/opt/android/android-studio/bin:${PATH}
-export PATH=${HOME}/Android/Sdk/platform-tools/:${PATH}
-export PATH=${HOME}/Android/Sdk/tools/:${PATH}
-export PATH=${HOME}/Android/Sdk/build-tools/${ANDROID_VER}/:${PATH}
+if [ -d /opt/android/android-studio ]; then
+    export PATH=/opt/android/android-studio/bin:${PATH}
+fi
+if [ -d ${HOME}/Android ]; then
+    export PATH=${HOME}/Android/Sdk/platform-tools/:${PATH}
+    export PATH=${HOME}/Android/Sdk/tools/:${PATH}
+    export PATH=${HOME}/Android/Sdk/build-tools/${ANDROID_VER}/:${PATH}
+fi
