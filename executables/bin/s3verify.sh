8e5dd306002c5896c36b2d05f2f47086744a5ee0
contradict <contradict@gmail.com>
2018-01-10 05:56:04 -0800

    s3verify skips files not in remote bucket
    prints warning instead of crashing

--- executables/bin/s3verify.sh
+++ executables/bin/s3verify.sh
@@ -2,14 +2,23 @@
 bucket=$1
 key_prefix=$2
 directory=$3
+partsize=64
+success=$(true)
 
 for x in $(ls ${directory}); do
     if ! real=$(aws s3api head-object \
                     --bucket ${bucket} \
                     --key ${key_prefix}/$x \
                     --query ETag 2>/dev/null); then
+        echo ${x} not in bucket
         continue
     fi;
-    local=$(./s3etag.sh ${directory}/$x 8);
-    echo $x $(echo $real | sed -e 's|["\\]||g') $local;
+    s3_tag=$(echo $real | sed -e 's|["\\]||g')
+    local_tag=$(s3etag.sh ${directory}/$x ${partsize});
+    if [ "${s3_tag}" != "${local_tag}" ]; then
+        success=$(false)
+        echo mismatch: ${x} ${s3_tag} ${local_tag};
+    fi
 done
+
+exit ${success}
