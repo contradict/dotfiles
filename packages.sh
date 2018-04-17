cdc957da10ab6a25b3958c3e11f4e4fc65302afe
contradict <contradict@gmail.com>
2018-04-16 17:40:01 -0700

    Add htop, virtualenv, tqdm. Fix python2 instalation

--- packages.sh
+++ packages.sh
@@ -3,7 +3,7 @@ if [ "0" = "`id -u`" ]; then
     apt-get install $(grep -vE "^\s*#" packages.txt | tr "\n" " ")
     wget -q https://bootstrap.pypa.io/get-pip.py
     python get-pip.py
-    pip install -r requirements.txt
+    pip2 install -r requirements.txt
     python3 get-pip.py
     pip3 install -r requirements.txt
     rm get-pip.py
