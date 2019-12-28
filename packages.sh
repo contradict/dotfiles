8dd5f3e80842440dae4ca1d53e36e06848cd969b
contradict <contradict@gmail.com>
2019-12-27 16:08:03 -0800

    Drop python2

--- packages.sh
+++ packages.sh
@@ -2,8 +2,6 @@
 if [ "0" = "`id -u`" ]; then
     apt-get install $(grep -vE "^\s*#" packages.txt | tr "\n" " ")
     wget -q https://bootstrap.pypa.io/get-pip.py
-    python get-pip.py
-    pip2 install -r requirements.txt
     python3 get-pip.py
     pip3 install -r requirements.txt
     rm get-pip.py
