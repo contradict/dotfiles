d370ed5286cf5f9d183ba3a5680f81bec64be370
contradict <contradict@gmail.com>
2020-01-12 12:25:56 -0800

    Fancier Julia startup script

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -1,11 +1,27 @@
 ENV["PYTHON"] = ""
 ENV["JULIA_REVISE_INCLUDE"] = 1
+
+using Logging
+
+macro install_and_use(pkgname)
+    return quote
+        local e
+        try
+            @eval using Pkg
+            haskey(Pkg.installed(), $(string(pkgname))) || @eval Pkg.add($(string(pkgname)))
+            @eval using $pkgname
+        catch e
+            @warn "Error at initial setup of " * $(string(pkgname)) * ": $(e.msg)"
+        end
+    end
+end
+
 atreplinit() do repl
+    @install_and_use Revise
     try
-        @eval using OhMyREPL
-        @eval using Revise
         @async Revise.wait_steal_repl_backend()
     catch e
-        print(e)
+        @warn "Unable to initialize Revise: $(e.msg)"
     end
+    @install_and_use OhMyREPL
 end
