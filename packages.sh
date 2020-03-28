9945f9873926f67d84324a836efc2c0ca7fc1c93
contradict <contradict@gmail.com>
2020-03-28 13:43:19 -0700

    Less interaction during install

--- packages.sh
+++ packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ "0" = "`id -u`" ]; then
-    apt-get install $(grep -vE "^\s*#" packages.txt | tr "\n" " ")
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get install -yq $(grep -vE "^\s*#" packages.txt | tr "\n" " ")
     wget -q https://bootstrap.pypa.io/get-pip.py
     python3 get-pip.py
     pip3 install -r requirements.txt
