243494b9f0f61c9e27a9f6af645464a8c1f9d81b
contradict <contradict@gmail.com>
2020-03-30 10:21:43 -0700

    Add custom terminfo for tmux

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ VIM=/usr/bin/vim
 VIMDIR=${HOME}/.vim
 VUNDLEDIR=${VIMDIR}/bundle/Vundle.vim
 
-easy_packages="bash gdb git readline tmux executables pylint julia"
+easy_packages="bash gdb git readline tmux executables pylint julia terminfo"
 
 install_easy() {
 	${STOW} ${STOWARGS} ${1}
