650e7c3e8cc010467c9a50ef5679aa2013e9d946
contradict <contradict@gmail.com>
2020-09-02 13:46:35 -0700

    Update before install for new machines

--- packages.sh
+++ packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ "0" = "`id -u`" ]; then
     export DEBIAN_FRONTEND=noninteractive
+    apt-get update
     apt-get install -yq $(grep -vE "^\s*#" packages.txt | tr "\n" " ")
     wget -q https://bootstrap.pypa.io/get-pip.py
     python3 get-pip.py
