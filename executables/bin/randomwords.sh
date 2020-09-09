92b3b8ad09b31c9dae6e0b6f3a8fb335553edc41
contradict <contradict@gmail.com>
2020-09-09 16:55:18 -0700

    Play with randomwords script

--- executables/bin/randomwords.sh
+++ executables/bin/randomwords.sh
@@ -3,28 +3,32 @@
 # Random Word Generator 
 # linuxconfig.org 
  
-if [ $# -ne 1 ] 
-then 
-    echo "Please specify how many random words would you like to generate !"
-    1>&2 
-    echo "example: ./random-word-generator 3" 1>&2 
-    echo "This will generate 3 random words" 1>&2 
-    exit 0
-fi 
- 
-# Constants 
+# Constants
 X=0
 ALL_NON_RANDOM_WORDS=/usr/share/dict/words
- 
-# total number of non-random words available 
-non_random_words=`cat $ALL_NON_RANDOM_WORDS | wc -l` 
- 
-# while loop to generate random words  
+
+# total number of non-random words available
+non_random_words=$(wc -l $ALL_NON_RANDOM_WORDS | cut -d' ' -f 1)
+
+ENTROPY=$(echo "a=l(${non_random_words})/l(2);scale=0;a/1" | bc -l)
+
+if [ $# -ne 1 ] 
+then 
+    echo "Please specify how many random words would you like to generate !" 1>&2
+    echo "example: ./random-word-generator 3" 1>&2 
+    echo "This will generate 3 random words" 1>&2 
+    echo "Each words has ~${ENTROPY} bits of entropy" 1>&2
+    exit 0
+fi
+
+randbytes=$(echo "a=(${ENTROPY}/8 + 1);scale=0;a/1" | bc)
+
+# while loop to generate random words
 # number of random generated words depends on supplied argument 
 while [ "$X" -lt "$1" ] 
 do 
-    random_number=`od -N3 -An -i /dev/urandom | 
-    awk -v f=0 -v r="$non_random_words" '{printf "%i\n", f + r * $1 / 16777216}'` 
-    sed `echo $random_number`"q;d" $ALL_NON_RANDOM_WORDS 
+    random_number=$(od -N"${randbytes}" -An -i /dev/urandom)
+    random_index=$(echo "${random_number} * ${non_random_words} / 2^(${randbytes}*8)" | bc)
+    sed "${random_index}q;d" $ALL_NON_RANDOM_WORDS
     let "X = X + 1" 
 done
