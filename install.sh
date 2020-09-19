265fa46451e0bfe40379504edffe35b1c0117637
contradict <contradict@gmail.com>
2020-09-18 17:56:57 -0700

    Fixup julia install

--- install.sh
+++ install.sh
@@ -13,39 +13,46 @@ VIM=/usr/bin/vim
 VIMDIR=${HOME}/.vim
 VUNDLEDIR=${VIMDIR}/bundle/Vundle.vim
 
-easy_packages="bash gdb git readline tmux executables pylint julia terminfo"
+easy_packages="bash gdb git readline tmux executables pylint terminfo"
 
 install_easy() {
-	${STOW} ${STOWARGS} ${1}
+	${STOW} "${STOWARGS}" "${1}"
 }
 
 install_vim() {
     install_easy vim_full
-    if [ -d ${VUNDLEDIR} ]; then
-        pushd ${VUNDLEDIR}
+    if [ -d "${VUNDLEDIR}" ]; then
+        pushd "${VUNDLEDIR}"
         git pull
         popd
         ${VIM} +PluginInstall! +qall
     else
-        git clone https://github.com/VundleVim/Vundle.vim.git ${VUNDLEDIR}
+        git clone https://github.com/VundleVim/Vundle.vim.git "${VUNDLEDIR}"
         ${VIM} +PluginInstall +qall
     fi
-    pushd ${VIMDIR}/bundle/YouCompleteMe
+    pushd "${VIMDIR}/bundle/YouCompleteMe"
     python3 install.py --clang-completer
     popd
+}
+
+install_julia() {
+    mkdir -p "${HOME}/.julia/config"
+    stow -t "${HOME}/.julia/config" julia
 }
 
 rm ~/.bash_logout ~/.bashrc ~/.profile
 
 for pkg in ${easy_packages}; do
-    install_easy $pkg
+    install_easy "$pkg"
 done
 pushd git && cp .gitconfig.template .gitconfig ; popd
 
-if [ -x "$(which vim)" -a -x "$(which git)" ] && vim --version | grep -q +python; then
+if [ -x "$(which vim)" ] && [ -x "$(which git)" ] && vim --version | grep -q +python; then
     echo "Installing complete vim config"
     install_vim
 else
     echo "Installing simple vim config, need git and vim with python support for full config"
     install_easy vim
 fi
+
+install_julia
--- packages.sh
+++ packages.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
-if [ "0" = "`id -u`" ]; then
+
+install_julia_executable() {
+    local JULIA_VERSION="1.5"
+    local JULIA_PATCH="1"
+    local JULIA_MACHINE
+    JULIA_MACHINE=$(uname -m)
+    case ${JULIA_MACHINE} in
+        x86_64)
+            JULIA_ARCH="x64"
+            ;;
+        aarch64)
+            JULIA_ARCH="aarch64"
+            ;;
+        *)
+            echo "No julia configuration known for machine=${JULIA_MACHINE}"
+            return
+            ;;
+    esac
+    JULIA_ARCH="x64"
+    JULIA_BASE_URL="https://julialang-s3.julialang.org/bin/linux/${JULIA_ARCH}/${JULIA_VERSION}/"
+    JULIA_ARCHIVE="julia-${JULIA_VERSION}.${JULIA_PATCH}-linux-${JULIA_MACHINE}.tar.gz"
+    cd /tmp || (echo "Unable to cd /tmp" && return)
+    curl -O "${JULIA_BASE_URL}${JULIA_ARCHIVE}"
+    tar -C /usr/local/stow -xzf "${JULIA_ARCHIVE}"
+    stow "julia-${JULIA_VERSION}.${JULIA_PATCH}"
+    cd - || (echo "Unable to restore working directory" && return)
+}
+
+if [ "0" = "$(id -u)" ]; then
     export DEBIAN_FRONTEND=noninteractive
     apt-get update
     apt-get install -yq $(grep -vE "^\s*#" packages.txt | tr "\n" " ")
@@ -7,6 +35,7 @@ if [ "0" = "`id -u`" ]; then
     python3 get-pip.py
     pip3 install -r requirements.txt
     rm get-pip.py
+    install_julia_executable
 else
     echo "Need root permissions"
 fi
