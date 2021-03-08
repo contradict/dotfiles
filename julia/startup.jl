f82becd010aedbe4d4c6334174f1b69eabfc223c
contradict <contradict@gmail.com>
2021-03-08 15:11:49 -0800

    Clean up julia startup script

--- julia/startup.jl
+++ julia/startup.jl
@@ -1,7 +1,7 @@
 ENV["PYTHON"] = ""
 ENV["JULIA_REVISE_INCLUDE"] = 1
 
-let history_file="./.repl_history.jl"
+let history_file=".repl_history.jl"
     project_path=dirname(Base.active_project())
     history_path=joinpath(project_path, history_file)
     if isfile(history_path) || islink(history_path)
@@ -9,17 +9,30 @@ let history_file="./.repl_history.jl"
     end
 end
 
-atreplinit() do repl
-    try
-        @eval using OhMyREPL
-        @eval using Revise
-        @async Revise.wait_steal_repl_backend()
-        # https://github.com/KristofferC/OhMyREPL.jl/issues/166
-        @async begin
-            sleep(2)
-            OhMyREPL.Prompt.insert_keybindings()
-        end
-    catch e
-        print(e)
+try
+    using Revise
+catch e
+    print(e)
+end
+
+try
+    using ImageInTerminal
+catch e
+    print(e)
+end
+
+try
+    using OhMyREPL
+catch e
+    print(e)
+end
+
+try
+    # https://github.com/KristofferC/OhMyREPL.jl/issues/166
+    @async begin
+        sleep(2)
+        OhMyREPL.Prompt.insert_keybindings()
     end
+catch e
+    print(e)
 end
