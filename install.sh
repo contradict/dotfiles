88d8aa7274f2d4b0c27e9c6a4dd7b15d84a0fa87
contradict <contradict@gmail.com>
2021-03-29 17:41:01 -0700

    Add option to install just one directory

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ VUNDLEDIR=${VIMDIR}/bundle/Vundle.vim
 easy_packages="bash gdb git readline tmux executables pylint terminfo"
 
 install_easy() {
-	${STOW} "${STOWARGS}" "${1}"
+    ${STOW} ${STOWARGS} "${1}"
 }
 
 install_vim() {
@@ -40,19 +40,57 @@ install_julia() {
     stow -t "${HOME}/.julia/config" julia
 }
 
-rm ~/.bash_logout ~/.bashrc ~/.profile
+install_fresh() {
+    rm ~/.bash_logout ~/.bashrc ~/.profile
 
-for pkg in ${easy_packages}; do
-    install_easy "$pkg"
+    for pkg in ${easy_packages}; do
+        install_easy "$pkg"
+    done
+    pushd git && cp .gitconfig.template .gitconfig ; popd
+
+    if [ -x "$(which vim)" ] && [ -x "$(which git)" ] && vim --version | grep -q +python; then
+        echo "Installing complete vim config"
+        install_vim
+    else
+        echo "Installing simple vim config, need git and vim with python support for full config"
+        install_easy vim
+    fi
+
+    install_julia
+}
+
+usage() {
+    echo "Install dotfiles."
+    echo "Must specify one of -f or -o <name>."
+    echo "  -f        Fresh install. Delete existing config and install this one."
+    echo "  -o <name> Install just the one set of config files under <name>."
+}
+
+while getopts ":hfo:" opt; do
+  case ${opt} in
+    h ) # process option h
+      usage
+      exit 0
+      ;;
+    f ) # process option f
+      install_fresh
+      exit 0
+      ;;
+    o )
+      install_easy ${OPTARG}
+      exit 0
+      ;;
+    \? )
+      echo Unknown option ${OPTARG}
+      usage
+      exit 1
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      usage
+      exit 1
+      ;;
+  esac
 done
-pushd git && cp .gitconfig.template .gitconfig ; popd
-
-if [ -x "$(which vim)" ] && [ -x "$(which git)" ] && vim --version | grep -q +python; then
-    echo "Installing complete vim config"
-    install_vim
-else
-    echo "Installing simple vim config, need git and vim with python support for full config"
-    install_easy vim
-fi
-
-install_julia
+usage
+exit 1
