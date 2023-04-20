b708497e77765fe6ccc9f76f92316dff9422d36e
contradict <contradict@gmail.com>
2023-04-20 11:32:44 -0700

    Clean up julia startup, add TerminalLoggers

--- julia/startup.jl
+++ julia/startup.jl
@@ -12,27 +12,40 @@ end
 try
     using Revise
 catch e
-    print(e)
+    @warn "Unable to enable Revise" e
 end
 
 try
     using ImageInTerminal
 catch e
-    print(e)
-end
-
-try
-    using OhMyREPL
-catch e
-    print(e)
+    @warn "Unable to enable ImageInTerminal" e
 end
 
 try
     # https://github.com/KristofferC/OhMyREPL.jl/issues/166
+    using OhMyREPL
     @async begin
-        sleep(2)
-        OhMyREPL.Prompt.insert_keybindings()
+        while !isdefined(Base, :active_repl)
+            sleep(0.1)
+        end
+        try
+            OhMyREPL.Prompt.insert_keybindings()
+        catch e
+            @warn "Unable to insert keybindings: " (e, catch_backtrace())
+        end
     end
 catch e
-    print(e)
+    @warn "Unable to enable OhMyREPL" e
+end
+
+atreplinit() do repl
+    try
+        @eval begin
+            using Logging: global_logger
+            using TerminalLoggers: TerminalLogger
+            global_logger(TerminalLogger())
+        end
+    catch e
+        @warn "Unable to enable TerminalLogger: " (e, catch_backtrace())
+    end
 end
