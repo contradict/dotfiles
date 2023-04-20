e2f4323c411679cacbd1fefe93e43dd4806ad9d7
contradict <contradict@gmail.com>
2023-04-20 11:35:20 -0700

    Update pyenv startup

--- bash/.rcscripts/Epyenv.sh
+++ bash/.rcscripts/Epyenv.sh
@@ -1,7 +1,9 @@
 if [ -d "$HOME/.pyenv" ]; then
     export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
+    export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+    command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
     if command -v pyenv 1>/dev/null 2>&1; then
         eval "$(pyenv init -)"
+        eval "$(pyenv virtualenv-init -)"
     fi
 fi
